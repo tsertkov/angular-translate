a250b2658064d56d62cc9064f0491039c6f42ad8
Aleksandr Tsertkov <tsertkov@gmail.com>
2015-06-11 16:51:14 +0200

    Handle null translations same as missing

--- src/service/translate.js
+++ src/service/translate.js
@@ -1424,7 +1424,7 @@ function $translate($STORAGE_KEY, $windowProvider, $translateSanitizationProvide
             Interpolator = (interpolationId) ? interpolatorHashMap[interpolationId] : defaultInterpolator;
 
         // if the translation id exists, we can just interpolate it
-        if (table && Object.prototype.hasOwnProperty.call(table, translationId)) {
+        if (table && Object.prototype.hasOwnProperty.call(table, translationId) && table[translationId] !== null) {
           var translation = table[translationId];
 
           // If using link, rerun $translate with linked translationId and return it
@@ -1483,7 +1483,7 @@ function $translate($STORAGE_KEY, $windowProvider, $translateSanitizationProvide
         }
 
         // if the translation id exists, we can just interpolate it
-        if (table && Object.prototype.hasOwnProperty.call(table, translationId)) {
+        if (table && Object.prototype.hasOwnProperty.call(table, translationId) && table[translationId] !== null) {
           var translation = table[translationId];
 
           // If using link, rerun $translate with linked translationId and return it
